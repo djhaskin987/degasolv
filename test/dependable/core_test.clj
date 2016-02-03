acf930d9ce74d0d02002b220202118ca0bf5007c
Daniel Jay Haskin <djhaskin987@gmail.com>
2016-02-02 21:38:46 -0700

    Clip. Refactor tests in preparation for adding another one.

--- test/dependable/core_test.clj
+++ test/dependable/core_test.clj
@@ -3,23 +3,38 @@
             [dependable.core :refer :all]))
 
 (deftest simple-find
-         (let [package-a
+         (let [package-a30
                {:name "a"
                 :version 30
                 :url "a_loc30"}
+               package-a20
+               {:name "a"
+                :version 20
+                :url "a_loc20"}
+               package-c10
+               {:name "c"
+                :version 10
+                :url "c_loc10"}
                repo-info
                {"a"
-                [package-a]}
-               query (fn [nm]
-                       (let [result (find repo-info nm)]
-                         (if (nil? result)
-                           :unsatisfiable
-                           (get result 1))))]
-           (testing (is (= (resolve-dependencies
-                                 ["a"]
-                                 query)
-                           [package-a])))
-           (testing (is (= (resolve-dependencies
-                             ["b"]
+                [package-a30
+                 package-a20]
+                "c"
+                [package-c10]}
+               query
+               (fn [nm]
+                 (let [result (find repo-info nm)]
+                   (if (nil? result)
+                     []
+                     (get result 1))))]
+           (testing "Asking for a present package succeeds."
+                    (is (= (resolve-dependencies
+                             [{:name "a"}]
                              query)
-                           [:unsatisfiable])))))
+                           [:successful [package-a30]])))
+           (testing "Asking for a nonexistent package fails."
+                    (is (= (resolve-dependencies
+                             [{:name "b"
+                               :trace :b-isnt-there}]
+                             query)
+                           [:unsatisfiable [:b-isnt-there]])))))
