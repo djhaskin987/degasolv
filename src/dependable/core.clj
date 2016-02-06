bd209f92e2bea3c0b6d8a18b5860eaea6ffae49e
Daniel Jay Haskin <djhaskin987@gmail.com>
2016-02-06 12:14:18 -0700

    Working on alr already-installed tests

--- src/dependable/core.clj
+++ src/dependable/core.clj
@@ -4,15 +4,27 @@
 
 (defn resolve-dependencies
   [names
-   query]
-  (reduce (fn [c v]
-            (let [result
-                  (query v)]
-                  (if (= result :unsatisfiable)
-                    (conj c result)
-                    (conj c (first result)))))
-          []
-          names))
+   query &
+   [:keys [already-installed]
+    :or {already-installed {}}]]
+  (loop [remaining (seq names)
+         installed already-installed
+         result [:successful]]
+    (if (empty? remaining)
+      result
+      (let [pkg (first remaining)
+            r (rest remaining)
+            pname (:name pkg)
+            response (query pname)]
+        (cond (empty? response) [:unsatisfiable pname]
+              (contains? installed pname) (recur r installed result)
+              :else
+              (let [chosen (first response)]
+                (recur r (assoc
+                           installed
+                           (:name chosen)
+                           (:version chosen))
+                       (conj result chosen))))))))
 
 #_(defn -main
   "I don't do a whole lot ... yet."
--- test/dependable/core_test.clj
+++ test/dependable/core_test.clj
@@ -2,39 +2,66 @@
   (:require [clojure.test :refer :all]
             [dependable.core :refer :all]))
 
-(deftest simple-find
-         (let [package-a30
-               {:name "a"
-                :version 30
-                :url "a_loc30"}
-               package-a20
-               {:name "a"
-                :version 20
-                :url "a_loc20"}
-               package-c10
-               {:name "c"
-                :version 10
-                :url "c_loc10"}
-               repo-info
-               {"a"
-                [package-a30
-                 package-a20]
-                "c"
-                [package-c10]}
-               query
-               (fn [nm]
-                 (let [result (find repo-info nm)]
-                   (if (nil? result)
-                     []
-                     (get result 1))))]
+(defn map-query [m]
+  (fn [nm]
+    (let [result (find m nm)]
+      (if (nil? result)
+        []
+        (let [[k v] result]
+          v)))))
+
+(let  [package-a30
+      {:name "a"
+       :version 30
+       :url "a_loc30"}
+      package-a20
+      {:name "a"
+       :version 20
+       :url "a_loc20"}
+      package-c10
+      {:name "c"
+       :version 10
+       :url "c_loc10"}
+      repo-info
+      {"a"
+       [package-a30
+        package-a20]
+       "c"
+       [package-c10]}
+       query (map-query repo-info)]
+  (deftest simple-find
            (testing "Asking for a present package succeeds."
                     (is (= (resolve-dependencies
                              [{:name "a"}]
                              query)
-                           [:successful [package-a30]])))
+                           [:successful package-a30])))
+                    (is (= (resolve-dependencies
+                             [{:name "c"}]
+                             query)
+                           [:successful package-c10]))
            (testing "Asking for a nonexistent package fails."
                     (is (= (resolve-dependencies
-                             [{:name "b"
-                               :trace :b-isnt-there}]
+                             [{:name "b"}]
                              query)
-                           [:unsatisfiable [:b-isnt-there]])))))
+                           [:unsatisfiable "b"]))))
+  (deftest already-installed
+           (testing "Asking to install a package twice."
+                    (is (= (resolve-dependencies
+                             [{:name "a"}
+                              {:name "a"}]
+                             query)
+                           [:successful package-a30])))
+           (testing (str "Asking to install a package that I have given as already "
+                    "installed.")
+                    (is (= (resolve-dependencies
+                             [{:name "c"}]
+                             :already-installed
+                             {"c" 18})
+                           [:successful])))
+           (testing (str "Asking to install a package that I have given as already "
+                         "installed, even though the package isn't availalbe.")
+                    (is (= (resolve-dependencies
+                             [{:name "b"}]
+                             :already-installed
+                             {"b" 30})
+                           [:successful])))))
