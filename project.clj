1c384b48f1d54e0aa6473443de2c198f216bb4fc
Daniel Haskin <djhaskin987@gmail.com>
2017-01-18 18:37:45 -0700

    Clip: point to cli file in lein project file

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/djhaskin987/dependable"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main dependable.cli
   :dependencies [
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
