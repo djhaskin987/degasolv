03f3fdb2bef9040e55a56f6415b6b295936b0b73
Daniel Haskin <djhaskin987@gmail.com>
2017-06-28 21:11:09 -0600

    Clip

--- artifactory-plugin/degasolv.groovy
+++ artifactory-plugin/degasolv.groovy
@@ -2,8 +2,11 @@
 // If your plugin requires any external dependencies, you can place them under
 // the ${ARTIFACTORY_HOME}/etc/plugins/lib directory.
 // -- https://www.jfrog.com/confluence/display/RTF/User+Plugins
-// So, you need to put this there:
+// So, you need to put these there:
 // https://mvnrepository.com/artifact/us.bpsm/edn-java/0.5.0
+// https://mvnrepository.com/artifact/org.apache.commons/commons-lang3/3.6
+// https://mvnrepository.com/artifact/org.codehaus.plexus/plexus-utils/3.0.24
+// https://mvnrepository.com/artifact/org.apache.maven/maven-artifact/3.5.0
 import us.bpsm.edn.parser.Parseable
 import us.bpsm.edn.parser.Parser
 import us.bpsm.edn.parser.Parsers
@@ -43,15 +46,14 @@ def recalculateIndex(dir) {
             lst.add(t)
         }
     }
+    // for each here that sorts each list using magics
+
     indexFile = new File('index.dsrepo')
     indexFile.withWriter('UTF-8') { writer ->
         writer.write(Printers.printString(
             Printers.defaultPrinterProtocol(),
             repoInfo))
     }
-//    println(m.get(newKeyword("x")).toString())
-//    println(m.get(newKeyword("y")).toString())
-//    println(Parser.END_OF_INPUT.equals(p.nextValue(pbr)))
 }
 
 recalculateIndex("./")
