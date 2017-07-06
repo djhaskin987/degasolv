1b8ec2386e14a02bc5c20122d8cf843acd9f845a
Daniel Haskin <djhaskin987@gmail.com>
2017-07-06 08:37:55 -0600

    Bump version number to 1.7.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject degasolv/degasolv "1.7.0"
+(defproject degasolv/degasolv "1.7.1-SNAPSHOT"
   :description "Dependency tracker with an eye toward building and shipping software."
   :url "http://github.com/djhaskin987/degasolv"
   :license {:name "Eclipse Public License"
