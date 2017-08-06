djhaskin987/degasolv

commit bbdf662cfa4d9c32e49ba265009b74928828ba2d

Fix test for inclusive conflict strat, tests that stuff is used if it already satisfies

src/degasolv/cli.clj
@@ -104,27 +104,31 @@
                    (string-to-requirement vetted-str-req)))
                requirements))
        present-packages
-       (into {}
-             (map
-               (fn [str-pkg]
-                 (let [vetted-str-pkg
-                       (s/conform ::r/frozen-package-string str-pkg)]
-                   (when (= vetted-str-pkg ::s/invalid)
-                     (binding [*out* *err*]
-                       (println
-                         (str
-                           "Present package string `"
-                           str-pkg
-                           "` invalid:"
-                           (s/explain ::r/frozen-package-string str-pkg)))))
-                   (let [[id version] (string/split str-pkg #"==")]
-                     [id
-                      (->PackageInfo
-                        id
-                        version
-                        "already present"
-                        nil)]))))
-         (:present-packages options))
+       (reduce
+        #(update-in %1 [(:id %2)]
+                    conj
+                    %2)
+        {}
+        (map
+         (fn [str-pkg]
+           (let [vetted-str-pkg
+                 (s/conform ::r/frozen-package-string str-pkg)]
+             (when (= vetted-str-pkg ::s/invalid)
+               (binding [*out* *err*]
+                 (println
+                  (str
+                   "Present package string `"
+                   str-pkg
+                   "` invalid:"
+                   (s/explain ::r/frozen-package-string str-pkg)))))
+             (let [[id version] (string/split str-pkg #"==")]
+               [id
+                (->PackageInfo
+                 id
+                 version
+                 "already present"
+                 nil)])))
+         (:present-packages options)))
        aggregate-repo
        (aggregate-repositories
          index-strat

src/degasolv/resolver_core.clj
@@ -105,7 +105,6 @@
            (or disj-cum
                (reduce
                 (fn [conj-cum conj-val]
-                  conj-val
                   (let [chk-ver (:version conj-val)
                         cmp-result (cmp pkg-ver chk-ver)]
                     (and conj-cum
@@ -170,11 +169,11 @@
           (concat (:absent partn) (:present partn) (:unspecified partn))]
       result)))
 
-                                        ; If transformed value passes test,
-                                        ; return a singleton list of those;
-                                        ; otherwise, return the full list of transformed stuffs.
-                                        ; This is because lazy seqs in clojure aren't exactly lazy;
-                                        ; they're chunked lazy, which is sad.
+;; If transformed value passes test,
+;; return a singleton list of those;
+;; otherwise, return the full list of transformed stuffs.
+;; This is because lazy seqs in clojure aren't exactly lazy;
+;; they're "chunked" lazy, which is sad. Sad panda :'(
 (defn- first-found
   [f pred coll]
   (reduce
@@ -186,6 +185,28 @@
          (conj c new-v))))
    []
    coll))
+
+;; does one of the present packages already
+;; satisfy criteria?
+(defn- present-packages-satisfies?
+  [present-id-packages
+   spec
+   safe-spec-call
+   status]
+  (reduce
+   #(or %1 %2)
+   false
+   (map
+    (fn [present-id-package]
+      (let [present-package-test
+            (safe-spec-call
+             spec
+             present-id-package)]
+        (or (and (= status :absent)
+                       (not present-package-test))
+                  (and (= status :present)
+                       present-package-test))))
+    present-id-packages)))
 
 (defn resolve-dependencies
   [requirements
@@ -224,9 +245,7 @@
                absent-specs
                clauses]
               (if (empty? clauses)
-                (if (= conflict-strat :inclusive)
-                  [:successful (set (flatten (vals found-packages)))]
-                  [:successful (set (vals found-packages))])
+                [:successful (set (flatten (vals found-packages)))]
                 (let [fclause (first clauses)
                       rclauses (subvec clauses 1)]
                   (if (empty? fclause)
@@ -243,23 +262,27 @@
                              [alternative]
                              (let [{status :status id :id spec :spec}
                                    alternative
-                                   present-package
+                                   present-id-packages
                                    (or (get present-packages id)
                                        (get found-packages id))]
                                (cond
-                                 (and (not (= conflict-strat :inclusive))
-                                      (not (nil? present-package)))
-                                 (if (or (= conflict-strat :prioritized)
-                                         (and (= status :absent)
-                                              (not (safe-spec-call spec present-package)))
-                                         (and (= status :present)
-                                              (safe-spec-call spec present-package)))
+                                 (and
+                                  (not
+                                   (nil? present-id-packages))
+                                  (or (= conflict-strat :prioritized)
+                                      (present-packages-satisfies?
+                                       present-id-packages
+                                       spec
+                                       safe-spec-call
+                                       status)))
                                    (resolve-deps
                                     repo
                                     present-packages
                                     found-packages
                                     absent-specs
                                     rclauses)
+                                   (and (not (nil? present-id-packages))
+                                        (not (= conflict-strat :inclusive)))
                                    [:unsuccessful
                                     {:problems
                                      [
@@ -269,7 +292,7 @@
                                        :absent-specs absent-specs
                                        :reason :present-package-conflict
                                        :alternative alternative
-                                       :package-id id}]}])
+                                       :package-id id}]}]
                                  (= status :absent)
                                  (resolve-deps
                                   repo
@@ -325,12 +348,9 @@
                                                 #(resolve-deps
                                                   repo
                                                   present-packages
-                                                  (if (= conflict-strat :inclusive)
-                                                    (update-in found-packages [id]
+                                                  (update-in found-packages [id]
                                                                conj
                                                                %)
-                                                    (assoc found-packages
-                                                           id %))
                                                   absent-specs
                                                   (into rclauses
                                                         (:requirements %)))

src/degasolv/resolver_utils.clj
@@ -111,7 +111,7 @@
       "Packages selected")]
     (when (not (nil? (:present-packages problem)))
       [(explain-package-list
-       (vals (:present-packages problem))
+       (flatten (vals (:present-packages problem)))
        "Packages already present")])
     (when (not (nil? (:alternative problem)))
       [(str "  - Alternative being considered: " (:alternative problem))])

test/degasolv/resolve_conflict_strat_test.clj
@@ -69,7 +69,7 @@
         (map-query repo-info)]
 
     (testing "A simple test of the inclusive conflict strat."
-      (is (.equals #{
+      (is (= #{
                  "http://example.com/repo/a-1.0.zip"
                  "http://example.com/repo/b-0.6.0.zip"
                  "http://example.com/repo/b-0.5.0.zip"
@@ -86,7 +86,7 @@
                     [:unsuccessful u]
                     :unsuccessful))))
     (testing "A test of the prioritized conflict strat."
-      (is (.equals #{
+      (is (= #{
                  "http://example.com/repo/a-1.0.zip"
                  "http://example.com/repo/b-0.6.0.zip"
                  "http://example.com/repo/c-0.2.3.zip"
@@ -102,7 +102,77 @@
                     [:unsuccessful u]
                     :unsuccessful))))))
 
-;; TODO: Test empty cases
-;; TODO: Test that it implies `fast`
-;; TODO: Test using absences
-;; TODO: Remove present-packages from the arg list
+(deftest inclusive-should-use-previously-found-stuff
+  (let [repo-info
+        {
+         "a"
+         [
+          {
+           :id "a"
+           :version "1.0"
+           :location "http://example.com/repo/a-1.0.zip"
+           :requirements
+           [
+            [
+             {:status :present
+              :id "b"
+              :spec [[{:relation :equal-to :version "0.6.0"}]]
+              }
+             ]
+            [
+             {:status :present
+              :id "c"
+              :spec [[{:relation :equal-to :version "0.2.3"}]]}
+             ]
+            ]
+           }]
+         "b"
+         [
+          {
+           :id "b"
+           :version "0.5.0"
+           :location "http://example.com/repo/b-0.5.0.zip"
+           :requirements []
+           }
+          {
+           :id "b"
+           :version "0.6.0"
+           :location "http://example.com/repo/b-0.6.0.zip"
+           :requirements []
+           }
+          ]
+         "c"
+         [
+          {
+           :id "c"
+           :version "0.2.3"
+           :location "http://example.com/repo/c-0.2.3.zip"
+           :requirements
+           [
+            [
+             {:status :present
+              :id "b"
+              :spec [[{:relation :greater-equal :version "0.5.0"}]]}
+             ]
+            ]
+           }
+          ]
+         }
+        query
+        (map-query repo-info)]
+    (testing "If a package matching criteria is already found, use present package."
+      (is (= #{
+                 "http://example.com/repo/a-1.0.zip"
+                 "http://example.com/repo/b-0.6.0.zip"
+                 "http://example.com/repo/c-0.2.3.zip"
+               }
+          (match
+           (resolve-dependencies
+                           [[{:status :present :id "a"}]]
+                           query
+                           :compare cmp
+                           :conflict-strat :inclusive)
+                    [:successful s]
+                    (set (map :location s))
+                    [:unsuccessful u]
+                    :unsuccessful))))))

test/degasolv/resolve_test.clj
@@ -131,7 +131,7 @@
                      ]
                     query
                     :present-packages
-                    {"c" package-c10}))))
+                    {"c" [package-c10]}))))
 
     (testing
         (str "Asking to install a package that I have given as already "
@@ -143,7 +143,7 @@
                      ]
                     query
                     :present-packages {"b"
-                                       (->package "b" 10 "b-loc10" nil)}))))
+                                       [(->package "b" 10 "b-loc10" nil)]}))))
     (testing
         (str "Asking to install a package that is already "
              "installed, but the installed version doesn't "
@@ -157,7 +157,7 @@
                    clause
                    ]
                   query
-                  :present-packages {"a" package-a20})))))
+                  :present-packages {"a" [package-a20]})))))
 
     (testing
         (str "Asking to install a package that is already "
@@ -168,7 +168,7 @@
                      [(present "a" #(>= (:version %) 20))]
                      ]
                     query
-                    :present-packages {"a" package-a20}))))))
+                    :present-packages {"a" [package-a20]}))))))
   (deftest ^:resolve-basic conflicts
     (let [dclause [(present "d")]]
       (testing
@@ -233,7 +233,7 @@
                      [(present "a")]
                      ]
                     query
-                    :present-packages {"b" package-b})))))
+                    :present-packages {"b" [package-b]})))))
   (let [package-a
         (->package
          "a"
@@ -352,7 +352,7 @@
                      [(absent "c") (present "b")]
                      ]
                     query
-                    :present-packages {"c" (->package "c" 10 "c_loc10" nil)}))))))
+                    :present-packages {"c" [(->package "c" 10 "c_loc10" nil)]}))))))
 
 (deftest ^:resolve-basic no-locking
   (testing
@@ -715,7 +715,7 @@
         (resolve-dependencies
          [[(present "a")]]
          query
-         :present-packages {"c" package-c}))))
+         :present-packages {"c" [package-c]}))))
     (testing
         "Prefer conflicts over installs"
       (is
@@ -725,7 +725,7 @@
         (resolve-dependencies
          [[(present "d")]]
          query
-         :present-packages {"c" package-c}
+         :present-packages {"c" [package-c]}
          :conflicts {"e" [nil]}))))))
 
 (deftest ^:resolve-basic circular-dependencies

test/degasolv/resolve_unsuccessful_test.clj
@@ -146,9 +146,9 @@
               {:term d-clause
                :alternative d-alternative
                :found-packages {"a"
-                                a1
+                                [a1]
                                 "b"
-                                b2
+                                [b2]
                                 }
                :present-packages {}
                :absent-specs {}
@@ -158,9 +158,9 @@
                :alternative d-alternative
                :found-packages {
                                 "a"
-                                a1
+                                [a1]
                                 "c"
-                                c3}
+                                [c3]}
                :present-packages {}
                :absent-specs {}
                :reason :package-not-found
@@ -178,9 +178,9 @@
               {:term d-clause
                :alternative d-alternative
                :found-packages {"a"
-                                a2
+                                [a2]
                                 "b"
-                                b2}
+                                [b2]}
                :present-packages {}
                :absent-specs {}
                :reason :package-not-found
@@ -188,9 +188,9 @@
               {:term d-clause
                :alternative d-alternative
                :found-packages {"a"
-                                a2
+                                [a2]
                                 "b"
-                                b3}
+                                [b3]}
                :present-packages {}
                :absent-specs {}
                :reason :package-not-found
@@ -205,7 +205,7 @@
             {:problems
              [{:term []
                :found-packages {
-                                "a" a3
+                                "a" [a3]
                                 }
                :present-packages {}
                :absent-specs {}

commit 763e958a0ae6d62367f07b65ee625fefb861e312

Fix bug introduced by bbdf662cfa4d9c32e49ba265009b74928828ba2d

src/degasolv/cli.clj
@@ -104,10 +104,13 @@
                    (string-to-requirement vetted-str-req)))
                requirements))
        present-packages
-       (reduce
-        #(update-in %1 [(:id %2)]
+       (t/spy :msg "present-packages"
+              (reduce
+               (fn package-aggregate
+                 [c [name pkg]]
+                 (update-in c [name]
                     conj
-                    %2)
+                    pkg))
         {}
         (map
          (fn [str-pkg]
@@ -128,7 +131,7 @@
                  version
                  "already present"
                  nil)])))
-         (:present-packages options)))
+         (:present-packages options))))
        aggregate-repo
        (aggregate-repositories
          index-strat