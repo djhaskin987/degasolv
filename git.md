2f0c9bd450078442416c8ec7dae121cdfaf8d3bc
Daniel Jay Haskin <djhaskin987@gmail.com>
2019-09-16 09:50:00 -0600

    Sparkle

---
+++ git.md
@@ -0,0 +1,7 @@
+One way to get a specific file:
+
+```
+git archive --format=zip --remote=ssh://<user>@<host>/repos/<repo name> <tag or HEAD> <filename> > <output file name>.zip
+```
+
+
--- src/degasolv/pkgsys/git.clj
+++ src/degasolv/pkgsys/git.clj
@@ -1,13 +1,53 @@
-(ns degasolv.pkgsys.git
-  "Namespace containing functions related to the git package system
-   integration point."
-  (:require [clojure.string :as string]
-            [clojure.java.shell :as sh]
-            [cheshire.core :as json]
-            [clojure.walk :as walk]
-            [miner.tagged :as tag]
-            [degasolv.util :refer :all]
-            [degasolv.resolver :as r :refer :all]))
+(require '[clj-http.client :as client]
+         '[cheshire.core :as json])
 
-(defn make-slurper [options]
-  )
+(defn all-tags [org]
+  (let
+    [baby (client/post
+    "https://api.github.com/graphql"
+    {:basic-auth ["djhaskin987" "77907ab111d65f085e2e80a79227d53bcf11c000"]
+     :content-type :json
+     :accept :json
+     :cookie-policy :standard
+     :body
+     (json/generate-string
+       {:variables
+        {:login "djhaskin987"
+         }
+        :query
+        (clojure.string/join
+          "\n"
+          [
+           "query RepoStuff($login: String!) {"
+           "  rateLimit {"
+           "    cost"
+           "    remaining"
+           "    resetAt"
+           "  }"
+           "  repositoryOwner(login: $login) {"
+           "    repositories(first: 100) {"
+           "      edges {"
+           "        cursor"
+           "        node {"
+           "          name"
+           "          refs(refPrefix: \"refs/tags/\", first: 100) {"
+           "            edges {"
+           "              cursor"
+           "              node {"
+           "                name"
+           "                target {"
+           "                  oid"
+           "                  ... on Tag {"
+           "                    name"
+           "                    message"
+           "                  }"
+           "                }"
+           "              }"
+           "            }"
+           "          }"
+           "        }"
+           "      }"
+           "    }"
+           "  }"
+           "}"
+           ])})}))
